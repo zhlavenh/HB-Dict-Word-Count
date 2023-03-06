eebff38b6f56ce354304053acc3b5d6af4aa2172
Zee Lavenhouse <Zeehlavenhouse@gmail.com>
2023-03-06 13:01:39 -0500

    Completed word count func

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,28 @@
 """Count words in file."""
 
+def word_count(filename):
 
-# put your code here.
+    file_name = open(filename, "r")
+
+    word_counts = {}
+    words = []
+
+    for line in file_name:
+        line = line.splitlines(keepends=False)
+        line = " ".join(line)
+        lines = line.split(" ")
+
+        words = words + lines
+
+    file_name.close()
+
+
+    for word in words:
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+    return word_counts
+
+print(word_count('twain.txt'))
+    
+
+
